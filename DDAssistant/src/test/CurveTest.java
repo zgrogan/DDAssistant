d30e300ad172997590d65070b2779003f86f9659
Zachary Grogan <zgrogan@yahoo.com>
2015-04-21 02:21:19 -0500

    minor change to CurveTest

--- DDAssistant/src/test/CurveTest.java
+++ DDAssistant/src/test/CurveTest.java
@@ -102,22 +102,4 @@ public class CurveTest {
 		assertTrue(ac.getInclinationAt(625) > 44 && ac.getInclinationAt(625) < 46);
 		assertTrue(ac.getInclinationAt(875) > 44 && ac.getInclinationAt(875) < 46);
 	}
-	/*
-	ActualCurve wd = new ActualCurve();
-	@Test
-	public void testaddSurvey() {
-		DDSurvey s1 = new DDSurvey(0,0,0);
-		DDSurvey s2 = new DDSurvey(250,0,0);
-		LinkedList<DDSurvey> lls = new LinkedList<DDSurvey>();
-		wd.addSurvey(0,0,0);
-		wd.addSurvey(250,0,0);
-		lls.add(s1);
-		lls.add(s2);
-		
-		assertTrue(lls.equals(wd.getData()));
-		
-		
-		
-	}
-*/
 }
