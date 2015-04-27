15e0957bf182e9b9c103b5a9b58ce1946e536566
Zachary Grogan <zgrogan@gmail.com>
2015-04-27 17:19:16 -0500

    refactor class names

---
+++ DDAssistant/src/ddassistant/NewFile.java
@@ -0,0 +1,62 @@
+package ddassistant;
+import java.sql.*;
+
+public class NewFile extends Connection
+{
+	protected PullFile NewWell = new PullFile();
+	private ResultSet resultSet;
+	PullFile pull = new PullFile();
+	protected String ID = new String();
+	String wellName = null;
+	Blob wellBlob;
+	
+	public boolean save()throws SQLException
+	{
+		boolean success = false;
+		int identifier;
+		boolean taken;
+		//getDataToStore();
+		
+		
+		////////////////////////////////////////////////////testing////////////////////////
+		wellName = "1123456789";//testing purposes well name
+		//wellBlob = 0b001001010;//testing purposes blob
+		////////////////////////////////////////////////////testing////////////////////////
+		
+		
+		taken = pull.checkAvailable(wellName);
+		if (!taken)
+		{
+			insertWellData(wellName,wellBlob);
+			success = true;
+		}
+		else
+		{
+			System.out.println("Name taken could not save.\nWill update file.");
+			updateWellData(wellName,wellBlob);
+		}
+			
+		return success;
+		
+	}
+	public void updateWellData(String wellName,Blob wellBlob)throws SQLException
+	{
+		mysql();
+		statement.execute("UPDATE well SET wellBlob='"+wellBlob+"' WHERE wellName="+wellName);
+		System.out.println("File udpate complete.");
+	}
+	public void insertWellData(String name,Blob wellBlob)throws SQLException
+	{
+		mysql();
+		statement.execute("INSERT INTO well(wellName,wellBlob) VALUES('"+name+"',"+wellBlob+")");
+		closure();
+		
+	}
+	public void getDataToStore()
+	{
+		//wellName = object.getWellName();//unhash once connected to main code
+		//wellBlob = ;//pull from main
+	}
+	//identifier = pull.getIdentifier(wellName);//use for pulling.
+
+}
---
+++ DDAssistant/src/ddassistant/PullFile.java
@@ -0,0 +1,61 @@
+package ddassistant;
+import java.sql.*;
+import java.util.*;
+
+public class PullFile extends Connection
+{
+	private ResultSet resultSet;
+	protected String ID = new String();
+	private ResultSetMetaData meta;
+	//private ArrayList<String> info;
+	private int count = 0;
+	private int identifier;
+	public <String> List<String> load()throws Exception
+	{
+		mysql();
+		resultSet = statement.executeQuery("SELECT COUNT(ID) FROM well");
+		resultSet.first();
+		int max = Integer.parseInt(resultSet.getString(1));
+		ArrayList<String> info = new ArrayList<String>(max);
+		for (int x=1;x<=max;x++)
+		{
+			resultSet = statement.executeQuery("SELECT wellName FROM well WHERE ID="+x);
+			resultSet.first();
+			info.add((String) resultSet.getString(1).toString());
+		}
+		return (List<String>)info;
+	}
+	public Blob load(String name)throws Exception
+	{
+		mysql();
+		resultSet = statement.executeQuery("SELECT wellBlob FROM well WHERE wellName ="+name);
+		Blob blob = resultSet.getBlob(1);
+		return blob;
+		
+		
+	}
+ 	public boolean checkAvailable(String name)throws SQLException
+	{
+		mysql();
+		boolean check = false;
+		resultSet = statement.executeQuery("SELECT COUNT(ID) FROM well");
+		resultSet.first();
+		int max = Integer.parseInt(resultSet.getString(1));
+		for (int x=1;x<=max;x++)
+		{
+			resultSet = statement.executeQuery("SELECT wellName FROM well WHERE ID="+x);
+			resultSet.first();
+			if(name.equals(resultSet.getObject(1).toString())){check = true;max=0;}
+		}
+		return check;
+	}
+	public int getIdentifier(String name)throws SQLException
+	{
+		resultSet = statement.executeQuery("SELECT ID FROM well WHERE well='"+name+'"');
+		while(resultSet.next())
+		{
+			identifier = Integer.parseInt(resultSet.getObject(1).toString());
+		}
+		return identifier;
+	}
+}
